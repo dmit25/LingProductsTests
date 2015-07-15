673b7a664ec3e9e71f16203a77beca4aef26b1fe
Dmitry Zobnin <dmit25@hotmail.com>
2015-07-15 10:33:49 +0300

    abbyy results parser implemented (Compreno SDK 2.0)

--- AbbyRdfParser/Program.cs
+++ AbbyRdfParser/Program.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 using Linguistics.Core.Enums;
-using Linguistics.Core.Extensions;
 using Linguistics.Core.Interfaces.CRF.Training;
 
 namespace AbbyRdfParser
@@ -18,73 +16,125 @@ namespace AbbyRdfParser
             var model = new EntitiesCorpus();
             var doc =
                 XDocument.Parse(
-                    File.ReadAllText(@"E:\Data\_DOWNLOADS\Results\testmodeleng_abby\testmodeleng_abby_rdf.xml"));
+                    File.ReadAllText(@"rdf_IE_2.0.xml"));
             var root = doc.Root;
             var rdfnamespace = XNamespace.Get("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+            var auxnamespace = XNamespace.Get("http://www.abbyy.com/ns/Aux#");
+            var basicentitynamespace = XNamespace.Get("http://www.abbyy.com/ns/BasicEntity#");
             var nodeId = rdfnamespace.GetName("nodeID");
             var resource = rdfnamespace.GetName("resource");
-            var auxnamespace = XNamespace.Get("http://www.abbyy.com/ns/Aux#");
-            var start = auxnamespace.GetName("AnnotationStart");
-            var end = auxnamespace.GetName("AnnotationEnd");
             var annotationsName = auxnamespace.GetName("TextAnnotations");
-            var annotationsText = auxnamespace.GetName("DocumentText");
+            var annotation = auxnamespace.GetName("InstanceAnnotation");
+            var start = auxnamespace.GetName("annotation_start");
+            var end = auxnamespace.GetName("annotation_end");
+            var instance = auxnamespace.GetName("instance");
+            var annotationsText = auxnamespace.GetName("document_text");
             var annotations = root.Element(annotationsName);
             var text = annotations.Element(annotationsText).Value;
             File.WriteAllText("testmodeleng_text_abby.txt", text);
             var objects = new Dictionary<string, Tuple<int, int>>();
-            foreach (var xElement in annotations.Elements(auxnamespace.GetName("ObjectAnnotation")))
+            var allEntities = new Dictionary<KeyValuePair<int, int>, NerTextEntity>();
+            var basicEntityTypes = new HashSet<string>();
+            foreach (var e in root.Elements())
             {
-                var idattr = xElement.Attribute(nodeId);
+                if (e.Name.Namespace == basicentitynamespace)
+                {
+                    basicEntityTypes.Add(e.Name.LocalName);
+                }
+            }
+            foreach (var basicEntityType in basicEntityTypes)
+            {
+                Console.WriteLine(basicEntityType);
+            }
+            foreach (var ann in annotations
+                .Elements(auxnamespace.GetName("instance_annotation"))
+                .Select(a => a.Element(annotation))
+                .Where(a => a != null))
+            {
+                var ints = ann.Element(instance);
+                var idattr = ints.Attribute(nodeId);
+                var begin = int.Parse(ann.Element(start).Value);
+                var endi = int.Parse(ann.Element(end).Value);
                 if (idattr != null)
                 {
                     var id = idattr.Value;
                     if (!objects.ContainsKey(id))
                     {
-                        objects.Add(id,
-                            Tuple.Create(int.Parse(xElement.Attribute(start).Value),
-                                int.Parse(xElement.Attribute(end).Value)));
-                    }
-                    else
-                    {
-                        //var old = objects[id];
-                        //objects[id] = Tuple.Create(old.Item1, int.Parse(xElement.Attribute(end).Value));
+
+                        if (begin != endi)
+                        {
+                            objects.Add(id, Tuple.Create(begin, endi));
+                        }
                     }
                 }
                 else
                 {
-                    var resAttr = xElement.Attribute(resource);
+                    var resAttr = ints.Attribute(resource);
                     if (resAttr != null)
                     {
-                        var uri = resAttr.Value;
-                        if (uri.Contains("Geo#"))
+                        NerType type = GetTypeFromUriAttr(resAttr.Value, string.Empty);
+                        if (type != NerType.Undefined)
                         {
-                            model.Entities.Add(new NerTextEntity(int.Parse(xElement.Attribute(start).Value),
-                                int.Parse(xElement.Attribute(end).Value), NerType.Location));
+                            AddEntityToModel(model, begin, endi, type, allEntities);
                         }
                     }
                 }
             }
-            model.ClearedText = File.ReadAllText("testmodeleng_text.txt");
+            model.ClearedText = text;
 
             foreach (var xElement in root.Elements())
             {
-                if (xElement.Name.Namespace.NamespaceName.EndsWith("Geo#"))
+                NerType type = GetTypeFromUriAttr(xElement.Name.Namespace.NamespaceName, xElement.Name.LocalName);
+                if (type != NerType.Undefined)
                 {
                     var idattr = xElement.Attribute(nodeId);
                     if (idattr != null)
                     {
                         var val = idattr.Value;
                         Tuple<int, int> pos;
-                        //if (objects.TryGetValue(val, out pos))
-                        //{
-                        //    model.Entities.Add(new NerTextEntity(pos.Item1, pos.Item2, NerType.Location));
-                        //}
+                        if (objects.TryGetValue(val, out pos))
+                        {
+                            AddEntityToModel(model, pos.Item1, pos.Item2, type, allEntities);
+                        }
                     }
                 }
             }
             File.WriteAllText("testmodeleng_abby.txt", model.Render());
-            model.ClearedText = text;
-            File.WriteAllText("testmodeleng_abby_native.txt", model.Render());
+            Console.ReadLine();
+        }
+
+        private static void AddEntityToModel(EntitiesCorpus model, int begin, int endi, NerType type, Dictionary<KeyValuePair<int, int>, NerTextEntity> allEntities)
+        {
+            NerTextEntity existing;
+            if (allEntities.TryGetValue(new KeyValuePair<int, int>(begin, endi), out existing) && existing.Type != type)
+            {
+                model.Entities.Remove(existing);
+            }
+            if (existing != null)
+            {
+                return;
+            }
+            var ent = new NerTextEntity(begin, endi, type);
+            model.Entities.Add(ent);
+            allEntities.Add(new KeyValuePair<int, int>(begin, endi), ent);
+        }
+
+        private static NerType GetTypeFromUriAttr(string namespaceName, string localName)
+        {
+            if (namespaceName.Contains("Geo#"))
+            {
+                return NerType.Location;
+            }
+
+            if (namespaceName.Contains("Org#"))
+            {
+                return NerType.Org;
+            }
+            if (localName == "Person")
+            {
+                return NerType.Person;
+            }
+            return NerType.Undefined;
         }
     }
 }
